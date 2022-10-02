472232c981c2695655701b863cc82e15c9321cf1
atakantekoglu <atakantekoglu@gmail.com>
2022-10-02 17:52:41 +0300

    organize the folder structure

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "@/pages/Home";
+import ThreadShow from "@/pages/ThreadShow";
+import NotFound from "@/pages/NotFound";
+
+export {
+    Home,
+    ThreadShow,
+    NotFound
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,4 @@
-import PageHome from "@/components/PageHome";
-import PageThreadShow from "@/components/PageThreadShow";
-import PageNotFound from "@/components/PageNotFound";
+import { Home, ThreadShow, NotFound } from "@/pages";
 import { createRouter, createWebHistory } from "vue-router";
 import sourceData from '@/data.json';
 
@@ -8,12 +6,12 @@ const routes = [
     {
         path: '/',
         name:'Home',
-        component: PageHome
+        component: Home
     },
     {
         path: '/thread/:id',
         name:'ThreadShow',
-        component: PageThreadShow,
+        component: ThreadShow,
         props: true,
         beforeEnter: (to, from, next) => {
             const threadExist = sourceData.threads.find((thread) => thread.id === to.params.id)
@@ -27,7 +25,7 @@ const routes = [
     {
         path: '/:pathMatch(.*)*',
         name: 'PageNotFound',
-        component: PageNotFound
+        component: NotFound
     },
 ]
 
