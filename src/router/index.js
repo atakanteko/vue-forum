2d7910671b56d83d4bfcc1c28a6185034d2bbd58
atakantekoglu <atakantekoglu@gmail.com>
2022-10-02 17:22:46 +0300

    redirect to 404 page with vue router without changing the url

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import PageHome from "@/components/PageHome";
 import PageThreadShow from "@/components/PageThreadShow";
 import PageNotFound from "@/components/PageNotFound";
-
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory } from "vue-router";
+import sourceData from '@/data.json';
 
 const routes = [
     {
@@ -14,7 +14,15 @@ const routes = [
         path: '/thread/:id',
         name:'ThreadShow',
         component: PageThreadShow,
-        props: true
+        props: true,
+        beforeEnter: (to, from, next) => {
+            const threadExist = sourceData.threads.find((thread) => thread.id === to.params.id)
+            if (threadExist){
+                return next();
+            } else{
+                next({ name:'PageNotFound', params: { pathMatch: to.path.substring(1).split('/') }})
+            }
+        },
     },
     {
         path: '/:pathMatch(.*)*',
